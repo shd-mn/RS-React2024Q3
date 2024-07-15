5c5596458cde5161f99e7fda2e6af446fe0a6417
Shadman <shadman.alizada@gmail.com>
2024-07-15 14:48:00 +0400

    feat: add react router dom, create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import Home from './container/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+import Details from './components/Details';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [{ path: '/', element: <Details /> }],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 function App() {
-  return <Home />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
